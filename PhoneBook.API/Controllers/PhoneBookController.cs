428eb1fca8c0e9ebc17dd2285de9a92e8892e17e
Gertian Mici <gertian.mici@gmail.com>
2021-02-05 13:19:11 +0100

    xml doc

--- PhoneBook.API/Controllers/PhoneBookController.cs
+++ PhoneBook.API/Controllers/PhoneBookController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using PhoneBook.Models.ViewModels;
 using PhoneBook.Core.Interfaces;
 
+
 namespace PhoneBook.API.Controllers
 {
     [Route("api/[controller]")]
@@ -13,13 +14,16 @@ namespace PhoneBook.API.Controllers
     public class PhoneBookController : ControllerBase
     {
         private readonly IPhoneBookRepository _phoneBookRepository;
-
+        /// <summary>
+        /// Add dependency injection through ctor
+        /// </summary>
+        /// <param name="phoneBookRepository"></param>
         public PhoneBookController(IPhoneBookRepository phoneBookRepository)
         {
             _phoneBookRepository = phoneBookRepository;
         }
         /// <summary>
-        /// Get method to get all users
+        /// method to get all users
         /// </summary>
         /// <param name="firstnameOrder"></param>
         /// <param name="ascOrder"></param>
@@ -34,7 +38,12 @@ namespace PhoneBook.API.Controllers
             var result = _phoneBookRepository.GetUsersOrdered(firstnameOrder, ascOrder, fileType);
             return result;
         }
-
+        /// <summary>
+        /// get specific user and his phone number by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fileType"></param>
+        /// <returns></returns>
         // GET api/<PhoneBookController>/5
         [HttpGet("{id}")]
         public GetPhoneBook Get(int id, int fileType = 0)
@@ -45,7 +54,12 @@ namespace PhoneBook.API.Controllers
             return result;
         }
         // GET api/values
-        [HttpGet("GetPhoneTypes")]
+        /// <summary>
+        /// Get all phone types from filetype
+        /// </summary>
+        /// <param name="fileType"></param>
+        /// <returns></returns>
+        [HttpGet("GetPhoneTypes")]        
         public IEnumerable<PhoneTypesVM> GetPhoneTypes(int fileType = 0)
         {
             if (fileType != 1 && fileType != 2 && fileType != 0)
@@ -54,6 +68,12 @@ namespace PhoneBook.API.Controllers
             return result;
         }
         // POST api/<PhoneBookController>
+        /// <summary>
+        /// post data to file
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="fileType"></param>
+        /// <returns></returns>
         [HttpPost]
         public GetPhoneBook Post([FromBody] PhoneBookVM user, int fileType = 0)
         {
@@ -62,6 +82,12 @@ namespace PhoneBook.API.Controllers
             var result = _phoneBookRepository.PostUser(user, fileType);
             return result;
         }
+        /// <summary>
+        /// add new numbers to existing users
+        /// </summary>
+        /// <param name="phoneBook"></param>
+        /// <param name="fileType"></param>
+        /// <returns>Check user data error if result is null</returns>
         [HttpPost("AddPhoneNumber")]
         public ActionResult<GetPhoneBook> AddPhoneNumber([FromBody] PhoneBookVM phoneBook, int fileType = 0)
         {
@@ -74,7 +100,13 @@ namespace PhoneBook.API.Controllers
             }
             return result;
         }
-
+        /// <summary>
+        /// Update specified user
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="user"></param>
+        /// <param name="fileType"></param>
+        /// <returns></returns>
         // PUT api/<PhoneBookController>/5
         [HttpPut("{id}")]
         public ActionResult<GetPhoneBook> Put(int id, [FromBody] PhoneBookVM user, int fileType = 0)
@@ -89,7 +121,12 @@ namespace PhoneBook.API.Controllers
 
             return result;
         }
-
+        /// <summary>
+        /// Delete specified user and hhis phone numbers
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fileType"></param>
+        /// <returns></returns>
         // DELETE api/<PhoneBookController>/5
         [HttpDelete("{id}")]
         public bool Delete(int id, int fileType = 0)
@@ -99,6 +136,12 @@ namespace PhoneBook.API.Controllers
             var result = _phoneBookRepository.DeleteUser(id, fileType);
             return result;
         }
+        /// <summary>
+        /// Add new phone types
+        /// </summary>
+        /// <param name="vm"></param>
+        /// <param name="fileType"></param>
+        /// <returns></returns>
         [HttpPost("AddPhoneTYPES")]
         public bool AddPhoneTypesVM([FromBody] PostPhoneTypesVM vm, int fileType = 0)
         {
